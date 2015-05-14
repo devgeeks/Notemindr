35e2ae8325228b8009e57a53113df2dd15ea0220
Tommy-Carlos Williams <tommy@devgeeks.org>
2015-05-14 20:53:53 +1000

    Linting tidy-up

--- src/utils/CryptonAPIUtils.js
+++ src/utils/CryptonAPIUtils.js
@@ -1,15 +1,18 @@
+'use strict';
+
 /* global crypton */
 
-var appConstants = require('../constants/appConstants');
+//var appConstants = require('../constants/appConstants');
 
 module.exports = {
-  authorize: function(username, passphrase) {
-    crypton.authorize(username, passphrase, function(err, session) {
+  authorize: (username, passphrase) => {
+    crypton.authorize(username, passphrase, (err, session) => {
       if (err) {
         // @TODO - Call an ERROR ACTION
         return;
       }
       // @TODO - Call a SUCCESS ACTION
+      console.log(session);
       return;
     });
   }
