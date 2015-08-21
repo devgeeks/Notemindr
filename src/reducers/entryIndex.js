370f45c34539df40f64ed08e02599eee167c6524
Tommy-Carlos Williams <tommy@devgeeks.org>
2015-08-21 17:31:23 +1000

    MPLv2 headers

--- src/reducers/entryIndex.js
+++ src/reducers/entryIndex.js
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 import {
   INDEX_LOAD_PENDING, INDEX_LOAD_SUCCESS, INDEX_LOAD_FAIL, INDEX_CACHED,
   INDEX_SAVE_PENDING, INDEX_SAVE_SUCCESS, INDEX_SAVE_FAIL, INDEX_FILTERED
